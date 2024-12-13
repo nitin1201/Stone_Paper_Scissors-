34d99a05122ea8479190055524b2de2296dffac2
Nitin sharma <114569518+nitin1201@users.noreply.github.com>
2024-12-13 11:03:56 +0530

    Updated

--- game.js
+++ game.js
@@ -2,7 +2,28 @@ const game = ["stone", "paper", "scissor"];
 const computerChoices = computerPicks();
 function computerPicks() {
   let randomChoice = Math.floor(Math.random() * game.length);
-  return game[randomChoice];
+  return randomChoice;
 }
 console.log(computerChoices);
-
+function user(choose) {
+  if (game[choose] == undefined || choose > 2) {
+    return "choose valid input";
+  }
+  console.log(`user choose: ${game[choose]}`);
+  console.log(`computer choose: ${game[computerPicks()]}`);
+  if (computerChoices == choose) {
+    return "match tie";
+  }
+  if (computerChoices == 0 && choose == 1) {
+    return "computer win";
+  }
+  if (computerChoices == 2 && choose == 1) {
+    return "computer win";
+  }
+  if (computerChoices == 0 && choose == 2) {
+    return "computer win";
+  }
+  return "user win";
+}
+let value = prompt("choose 0 for stone 1 for paper 2 for scissor");
+console.log(user(value));
